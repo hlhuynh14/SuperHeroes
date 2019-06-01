cd6eda007d609daf8281388295fad552fd680c79
henry huynh <hlhuynh14@gmail.com>
2019-06-01 18:55:00 -0500

    create superhero

---
+++ SuperHeroes/Controllers/SuperheroesController.cs
@@ -0,0 +1,97 @@
+﻿using SuperHeroes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperHeroes.Controllers
+{
+    public class SuperheroesController : Controller
+    {
+        ApplicationDbContext context;
+        public SuperheroesController()
+        {
+            context = new ApplicationDbContext();
+        }
+        // GET: Superheroes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Superheroes/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Superheroes/Create
+        public ActionResult Create()
+        { SuperHero superHero = new SuperHero();
+            return View(superHero);
+        }
+
+        // POST: Superheroes/Create
+        [HttpPost]
+        public ActionResult Create(SuperHero superHero)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                context.superHeroes.Add(superHero);
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Superheroes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Superheroes/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Superheroes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Superheroes/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SuperHeroes/Models/SuperHero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeroes.Models
+{
+    public class SuperHero
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string alterEgo { get; set; }
+        public string primaryAbilies { get; set; }
+        public string secondaryAbilities { get; set; }
+        public string catchphrase { get; set; }
+    }
+}
